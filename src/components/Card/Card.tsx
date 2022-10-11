d8133827d0ef6925e15359f58672fd619e4bf347
moneyandfame7 <davidoo1234e@gmail.com>
2022-10-11 08:48:27 +0300

    added the react-skeleton-loader library and created a function that creates a fake array

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -3,6 +3,7 @@ import BtnAdd from '../buttons/Add'
 import React, {useState} from 'react'
 import BtnAdded from '../buttons/Added'
 import {IShopItem} from '../../App'
+import ContentLoader from 'react-content-loader'
 
 export interface ICardProps {
     name: string
@@ -12,9 +13,23 @@ export interface ICardProps {
     onPlus?: (obj: IShopItem) => void
     id: string
     favorited?: boolean
+    added?: boolean
+    loading?: boolean
 }
 
-const Card: React.FC<ICardProps> = ({id, name, price, image, onFavorite, onPlus, favorited = false}) => {
+const Card: React.FC<ICardProps> = ({
+                                        id,
+                                        name,
+                                        price,
+                                        image,
+                                        loading = false,
+                                        onFavorite,
+                                        onPlus,
+                                        favorited = false,
+                                        added = false
+                                    }) => {
+    const [isAdded, setAdded] = useState(added)
+    const [isFavorite, setFavorite] = useState(favorited)
     const onClickPlus = (): void => {
         onPlus && onPlus({name, price, image, id})
         isAdded ? setAdded(false) : setAdded(true)
@@ -23,25 +38,43 @@ const Card: React.FC<ICardProps> = ({id, name, price, image, onFavorite, onPlus,
         onFavorite && onFavorite({name, price, image, id})
         setFavorite(!isFavorite)
     }
-    const [isAdded, setAdded] = useState(false)
-    const [isFavorite, setFavorite] = useState(favorited)
+
     return (
         <div className={styles.card}>
-            <img className={styles.favourite}
-                 src={isFavorite ? '/img/favourite.svg' : '/img/unfavourite.svg'}
-                 alt="Favourite"
-                 onClick={onClickFavorite}
-            />
-            <img width={133} height={112} src={image} alt="Sneakers"/>
-            <h5>{name}</h5>
-            <div className="d-flex justify-between align-center">
-                <div className="d-flex flex-column">
-                    <span>Ціна:</span>
-                    <b>{price.toLocaleString('ua')} грн.</b>
-                </div>
+            {
+                loading ? <ContentLoader
+                        speed={2}
+                        width={155}
+                        height={265}
+                        viewBox="0 0 150 265"
+                        backgroundColor="#f3f3f3"
+                        foregroundColor="#ecebeb">
+                        <rect x="1" y="0" rx="10" ry="10" width="155 " height="155"/>
+                        <rect x="0" y="167" rx="5" ry="5" width="155" height="15"/>
+                        <rect x="0" y="187" rx="5" ry="5" width="100" height="15"/>
+                        <rect x="1" y="234" rx="5" ry="5" width="80" height="25"/>
+                        <rect x="124" y="230" rx="10" ry="10" width="32" height="32"/>
+                    </ContentLoader>
+                    :
+                    <>
+                        <img className={styles.favourite}
+                             src={isFavorite ? '/img/favourite.svg' : '/img/unfavourite.svg'}
+                             alt="Favourite"
+                             onClick={onClickFavorite}
+                        />
+                        <img width={133} height={112} src={image} alt="Sneakers"/>
+                        <h5>{name}</h5>
+                        <div className="d-flex justify-between align-center">
+                            <div className="d-flex flex-column">
+                                <span>Ціна:</span>
+                                <b>{price.toLocaleString('ua')} грн.</b>
+                            </div>
+                            {!isAdded && <BtnAdd handlerButton={onClickPlus}/> ||
+                                <BtnAdded handlerButton={onClickPlus}/>}
+                        </div>
+                    </>
 
-                {!isAdded && <BtnAdd handlerButton={onClickPlus}/> || <BtnAdded handlerButton={onClickPlus}/>}
-            </div>
+            }
         </div>
     )
 }
